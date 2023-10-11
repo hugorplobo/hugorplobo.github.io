61907aff60bd9ef3907722dd21e719a071ca36b3
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-11 01:48:57 -0300

    feat: add title id and underlined effect on subtitles

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -2,15 +2,16 @@ import { ReactNode } from "react";
 import classes from "./style.module.scss";
 
 type Props = {
+  id: string,
   title: string,
   children: ReactNode,
 }
 
-export function Container({ title, children }: Props) {
+export function Container({ id, title, children }: Props) {
   return (
     <div className={classes.container}>
       <div className={classes.containerInner}>
-        <h1>{ title }</h1>
+        <h1 id={id}>{ title }</h1>
         { children }
       </div>
     </div>
