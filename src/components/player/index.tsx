9a97a51b47d18bd48dd2bd02c958f280582e5437
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-11 01:46:59 -0300

    feat: add music player component

---
+++ src/components/player/index.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import YTPlayer from "yt-player";
+import classes from "./style.module.scss";
+
+export function Player() {
+  const [player] = useState(new YTPlayer("#player"));
+  
+  useEffect(() => {
+    player.load("lP26UCnoH9s");
+    player.setVolume(10);
+  });
+
+  function onPlay() {
+    player.play();
+  }
+
+  function onPause() {
+    player.pause();
+  }
+
+  return (
+    <div className={classes.player}>
+      <div className={classes.controls}>
+        <button onClick={onPlay}>▶️</button>
+        <button onClick={onPause}>⏸️</button>
+        <p>Coffee Shop Radio</p>
+      </div>
+    </div>
+  );
+}
